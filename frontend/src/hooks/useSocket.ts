0b5a07108062c8b75f4c9a2d9c4742ef9d508ef2
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-11-06 21:25:05 +0500

    Create hook for socket

---
+++ frontend/src/hooks/useSocket.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const WS_URL = "http://localhost:8080";
+
+const useSocket = () => {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+
+  useEffect(() => {
+    const ws = new WebSocket(WS_URL);
+
+    ws.onopen = () => {
+      console.log("Connected");
+      setSocket(ws);
+    };
+
+    ws.onclose = () => {
+      console.log("Disconnect");
+      setSocket(null);
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  return socket;
+};
+
+export default useSocket;
