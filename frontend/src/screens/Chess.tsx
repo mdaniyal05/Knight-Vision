a3e0802f4064ebbde16e11cfa03f8ada5b25d058
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-11-06 22:19:45 +0500

    Create socket logic for init chess

--- frontend/src/screens/Chess.tsx
+++ frontend/src/screens/Chess.tsx
@@ -1,5 +1,50 @@
+import { useEffect } from "react";
+import Button from "../components/Button";
+import useSocket from "../hooks/useSocket";
+
+const INIT_GAME = "init_game";
+
 const Chess = () => {
-  return <div>Chess</div>;
+  const socket = useSocket();
+
+  useEffect(() => {
+    if (!socket) {
+      return;
+    }
+
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+
+      console.log(message);
+
+      switch (message.type) {
+        case INIT_GAME:
+          console.log("GAME INITIALIZED");
+          break;
+        default:
+          break;
+      }
+    };
+  }, [socket]);
+
+  if (!socket) {
+    return <p>Connecting....</p>;
+  }
+
+  return (
+    <>
+      <Button
+        onClick={() =>
+          socket.send(
+            JSON.stringify({
+              type: INIT_GAME,
+            })
+          )
+        }
+        buttonText="PLAY"
+      />
+    </>
+  );
 };
 
 export default Chess;
